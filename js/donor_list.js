fb9a8141f3e8bec6a4e871027396bb277b19bf1e
Sarthak <sarthaksri017@gmail.com>
2023-02-25 23:29:35 +0530

    ss

--- js/donor_list.js
+++ js/donor_list.js
@@ -8,7 +8,7 @@ $.ajax(
             window.localStorage.setItem('temp_data', JSON.stringify(data));
             let data3 = JSON.parse(window.localStorage.getItem('temp_data'));
             var count = data3.count;
-            var dat = "";
+            var da="";
             for (var i = 0; i < count; i++) {
                 var no = "No";
                 if (data3[i].probable_donor == 1) {
@@ -24,10 +24,9 @@ $.ajax(
                 <td>`+ data3[i].times_donated + `</td>
                  <td>`+ no + `</td>
             </tr>`;
-                dat += ht;
-
+               da+=ht;
             }
-            document.getElementById("table_donor").innerHTML+= dat;
+            document.getElementById("table_donor").innerHTML += da;
         },
         error: function (e) {
             alert('Error in putting data at     : ' + JSON.stringify(e));
@@ -36,5 +35,57 @@ $.ajax(
     });
 function filter()
 {
-    
+    var da=` <tr>
+    <th>Sr.no</th>
+    <th>Name</th>
+    <th>Blood Group</th>
+    <th>Gender</th>
+    <th>Last Donation</th>
+    <th>Contact Number</th>
+    <th>Times Donated</th>
+    <th>Might Donate</th>
+</tr>`;
+    var btype=document.getElementById("selectPointOfInterest").value;
+    $.ajax(
+        {
+            url: "../php/get_donors.php",
+            dataType: "json",
+            success: function (data) {
+                //data = JSON.parse(data);
+                window.localStorage.setItem('temp_data', JSON.stringify(data));
+                let data3 = JSON.parse(window.localStorage.getItem('temp_data'));
+                var count = data3.count;var c=1;
+                for (var i = 0; i < count; i++) {
+                    var no = "No";
+                    if (data3[i].probable_donor == 1) {
+                        no = "Yes";
+                    }
+                    if(data3[i].blood_group!=btype)
+                    {
+                        if(btype=="None")
+                        {
+
+                        }
+                        else
+                        continue;
+                    }
+                    let ht = `<tr>
+                    <td >`+ c++ + `.</td>
+                    <td>`+ data3[i].name + `</td>
+                    <td>`+ data3[i].blood_group + `</td>
+                    <td>`+ data3[i].gender + `</td>
+                    <td>`+ data3[i].last_donation_date + `</td>
+                    <td>`+ data3[i].ph_no + `</td>
+                    <td>`+ data3[i].times_donated + `</td>
+                     <td>`+ no + `</td>
+                </tr>`;
+                da+=ht;
+                }
+                document.getElementById("table_donor").innerHTML = da;
+            },
+            error: function (e) {
+                alert('Error in putting data at     : ' + JSON.stringify(e));
+                console.log("stcks=" + JSON.stringify(e));
+            }
+        });
 }
