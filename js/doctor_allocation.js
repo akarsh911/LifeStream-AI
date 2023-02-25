cf4546125caf2faba586d0ea4fd3739570278f1c
Sarthak <sarthaksri017@gmail.com>
2023-02-26 02:46:18 +0530

    ss

--- js/doctor_allocation.js
+++ js/doctor_allocation.js
@@ -5,7 +5,7 @@ for (var i = 0; i < count; i++) {
 
     let ht = `<div class="card">
                 <div class="card-image">
-                    <img src="../media/columbia-asia-hospital-patiala-patiala-1485238179-5886efa3ea671.jpeg" alt="">
+                    <img href="../html/doctors_list.html?s_no="`+data3[i].s_no+` src="`+data3[i].url+`" alt="">
                 </div>
                 <div class="category"> Contact:`+ data3[i].ph_no + `</div>
                 <div class="heading">`+ data3[i].name + ` <br>
