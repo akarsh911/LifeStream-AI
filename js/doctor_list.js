09ec1ec46d78a2bdeb9f4c210d4a349a0a8d8103
Sarthak <sarthaksri017@gmail.com>
2023-02-26 03:10:52 +0530

    ss

--- js/doctor_list.js
+++ js/doctor_list.js
@@ -3,11 +3,11 @@ var count = data3.count;
 var da = "";
 for (var i = 0; i < count; i++) {
 
-    let ht = ` <div class="card">
-    <img src="../media/istockphoto-1327024466-170667a.jpeg" alt="John" style="width:100%">
-    <h1>John Doe</h1>
-    <p class="title">CEO & Founder, Example</p>
-    <p>Harvard University</p>
+    let ht = `<div class="card">
+    <img src="`+data3[i].url+`" alt="John" style="width:100%">
+    <h1>`+data3[i].name+`</h1>
+    <p class="title">Email: `+data3[i].email+`</p>
+    <p>Contact: `+data3[i].ph_no+`</p>
     <p><button>Contact</button></p>
   </div>`;
     da += ht;
