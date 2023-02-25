968e08bd715e84b5ed1a8a27fe1bb7bae663f651
akarsh <akarshagri121@gmail.com>
2023-02-25 16:06:25 +0530

    first commit

---
+++ php/database_get_data.php
@@ -0,0 +1,98 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_connect.php");
+function check_user_exists($email)
+{
+    $conn = openCon();
+    $sql = "SELECT f_name FROM users WHERE email='$email' || ph_no='$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            return 1;
+        }
+    } else {
+        return 0;
+    }
+}
+function get_user_state($email)
+{
+    $conn = openCon();
+    $sql = "SELECT user_state FROM users WHERE email='$email' || ph_no='$email'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            return $row['user_state'];
+        }
+    } else {
+        return 0;
+    }
+}
+
+function get_email($email)
+{
+    $conn = openCon();
+    $sql = "SELECT email FROM users WHERE email='$email' || ph_no='$email'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            return $row['email'];
+        }
+    } else {
+        return 0;
+    }
+}
+function get_donors()
+{
+    $conn = openCon();
+    $sql = "SELECT * FROM donors_list";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $arr = array(
+                "name" => $row["name"], "ph_no" =>   $row["ph_no"], "age" => $row["age"], "blood_group" => $row["blood_group"], "gender" => $row["gender"],
+                "last_donation_date" => $row["last_donation_date"], "times_donated" => $row["times_donated"], "probable_donor" => $row["probable_donor"],
+            );
+            $json = json_encode($arr);
+            return $json;
+        }
+    } else {
+        return "nf";
+    }
+}
+function get_doctors()
+{
+    $conn = openCon();
+    $sql = "SELECT * FROM doctors_list";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $arr = array(
+             "name" => $row["name"], "ph_no" =>   $row["ph_no"],
+             "email" => $row["email"], "hospital_id" => $row["hospital_id"], "gender" => $row["gender"] 
+            );
+            $json = json_encode($arr);
+            return $json;
+        }
+    } else {
+        return "nf";
+    }
+}
+function get_hospitals()
+{
+    $conn = openCon();
+    $sql = "SELECT * FROM hospitals_list";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $arr = array(
+                "s_no" => $row["s_no"],"name" => $row["name"], "patients" =>   $row["patients"],
+                "doctors" => $row["doctors"]
+            );
+            $json = json_encode($arr);
+            return $json;
+        }
+    } else {
+        return "nf";
+    }
+}
---
+++ php/database_set_data.php
@@ -0,0 +1,33 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_connect.php");
+function create_user($username, $email, $f_name, $l_name, $ph_no, $psw_hash)
+{
+    $username = $email;
+    $conn = openCon();
+    $sql = "INSERT INTO users (username,f_name,l_name,email,ph_no,psw_hash,user_state) VALUES ('$username','$f_name','$l_name','$email','$ph_no','$psw_hash','user_first')";
+    if ($conn->query($sql) === TRUE) {
+        return 1;
+    } else {
+        return "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+
+function login($email, $psw_hash)
+{
+    $conn = openCon();
+    $sql = "SELECT * FROM users WHERE (email='$email' || ph_no='$email') && psw_hash='$psw_hash'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $arr = array(
+                "username" => $row["username"], "f_name" =>   $row["f_name"], "l_name" => $row["l_name"], "email" => $row["email"], "ph_no" => $row["ph_no"],
+                  "user_state" => $row["user_state"]
+            );
+            $json = json_encode($arr);
+            return $json;
+        }
+    } else {
+        return "nf";
+    }
+}
