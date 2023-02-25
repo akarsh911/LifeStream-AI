bf826577c4c9dba101812b260ab184b6f11a961c
Sarthak <sarthaksri017@gmail.com>
2023-02-26 02:57:37 +0530

    ss

--- js/doctor_allocation.js
+++ js/doctor_allocation.js
@@ -5,7 +5,7 @@ for (var i = 0; i < count; i++) {
 
     let ht = `<div class="card">
                 <div class="card-image">
-                    <img href="../html/doctors_list.html?s_no="`+data3[i].s_no+` src="`+data3[i].url+`" alt="">
+                <a href="../html/doctors_list.html?s_no=`+data3[i].s_no+`"> <img  src="`+data3[i].url+`" alt=""></a>
                 </div>
                 <div class="category"> Contact:`+ data3[i].ph_no + `</div>
                 <div class="heading">`+ data3[i].name + ` <br>
