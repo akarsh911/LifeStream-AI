b256a324e2674eabac942153f00ba59b75c8f878
akarsh <akarshagri121@gmail.com>
2023-02-26 11:55:48 +0530

    aks

--- js/user_access_level.js
+++ js/user_access_level.js
@@ -1,7 +1,10 @@
 let data3 = JSON.parse(window.localStorage.getItem('user_data'));
 var user = data3.user_type;
 console.log(data3);
-if (user == 2) {
+if (user == 0) {
+
+}
+else if (user == 2) {
     document.getElementById("pbr").style.visibility = "collapse";
     document.getElementById("log").style.visibility = "collapse";
     document.getElementById("doc").style.visibility = "collapse";
@@ -11,7 +14,7 @@ if (user == 2) {
     document.getElementById("log").style.display = "none";
     document.getElementById("doc").style.display = "none";
 }
-if (user == 3) {
+else if (user == 3) {
     document.getElementById("pbr").style.visibility = "collapse";
     document.getElementById("log").style.visibility = "collapse";
     document.getElementById("ambu").style.visibility = "collapse";
@@ -19,9 +22,20 @@ if (user == 3) {
     document.getElementById("ambu").style.display = "none";
     document.getElementById("log").style.display = "none";
 }
-if (user == 1) {
+else if (user == 1) {
     document.getElementById("pbr").style.visibility = "collapse";
     document.getElementById("doc").style.visibility = "collapse";
     document.getElementById("pbr").style.display = "none";
+    document.getElementById("doc").style.display = "none";
+}
+else {
+    document.getElementById("pbr").style.display = "none";
+    document.getElementById("doc").style.display = "none";
+    document.getElementById("pbr").style.display = "none";
+    document.getElementById("ambu").style.display = "none";
+    document.getElementById("log").style.display = "none";
+    document.getElementById("pbr").style.display = "none";
+    document.getElementById("ambu").style.display = "none";
+    document.getElementById("log").style.display = "none";
     document.getElementById("doc").style.display = "none";
 }
