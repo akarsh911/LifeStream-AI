909d14a51cea2e54eb582313d2f3fd524744bef8
akarsh <akarshagri121@gmail.com>
2023-02-26 05:50:18 +0530

    aks

--- php/get_blood_inventory.php
+++ php/get_blood_inventory.php
@@ -10,7 +10,6 @@ $c = 0;
 for ($i = 0; $i < $hospital->count; $i++) {
     $dis_ratio = ($hospital->$i->patients) / $pat_no;
     echo json_encode($hospital->$i);
-    $new_arr;
     $new_arr->OP = round($dis_ratio * $blood[0]->OP);
     $new_arr->OM = round($dis_ratio * $blood[0]->OM);
     $new_arr->AP = round($dis_ratio * $blood[0]->AP);
@@ -19,7 +18,7 @@ for ($i = 0; $i < $hospital->count; $i++) {
     $new_arr->BM = round($dis_ratio * $blood[0]->BM);
     $new_arr->ABP = round($dis_ratio * $blood[0]->ABP);
     $new_arr->ABM = round($dis_ratio * $blood[0]->ABM);
-    array_push($hospital->$i,$new_arr);
+    array_push($hospital->$i, $new_arr);
 }
 $blood = json_encode($blood);
 $hospital = json_encode($hospital);
