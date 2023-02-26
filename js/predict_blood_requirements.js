0e10dbd3bbbe0d3d3788576a479345abdc16fe63
akarsh <akarshagri121@gmail.com>
2023-02-26 10:11:12 +0530

    aks latest

---
+++ js/predict_blood_requirements.js
@@ -0,0 +1,18 @@
+let data3 = JSON.parse(window.localStorage.getItem('hospital_blood_distribution'));
+var count = data3.count;
+var da = "";
+
+for (var i = 0; i < count; i++) {
+
+    let ht = `<div class="card">
+                <div class="card-image">
+                <a href="../html/doctors_list.html?s_no=`+data3[i].s_no+`"> <img  src="`+data3[i].url+`" alt=""></a>
+                </div>
+                <div class="category"> Contact:`+ data3[i].ph_no + `</div>
+                <div class="heading">`+ data3[i].name + ` <br>
+                    <span class="author">No. of patients-`+ data3[i].patients + `</span> <br>
+                    <span class="author">No. of doctors-`+ data3[i].doctors + `</span> <br>
+                </div></div>`;
+    da += ht;
+}
+document.getElementById("container").innerHTML += da;
